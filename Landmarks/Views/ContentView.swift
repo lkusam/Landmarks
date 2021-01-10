a7e8397b47acfb7f8887371faa53dd5ef455fe84
lkusam <lkusam@kent.edu>
2021-01-09 22:57:43 -0500

    Added navigation to the home page

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -8,8 +8,33 @@ A view showing the list of landmarks.
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection: Tab = .featured
+    
+    enum Tab {
+        case featured
+        case list
+    }
+    
     var body: some View {
-        LandmarkList()
+        
+        TabView(selection: $selection){
+            CategoryHome()
+                .tabItem { Label("Featured", systemImage: "star") }
+                .tag(Tab.featured)
+            
+            LandmarkList()
+                .tabItem {
+                Label("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
+            
+            Text("Hello man")
+                .foregroundColor(.green)
+                .tabItem { Label("Man this is text", systemImage: "phone.circle.fill" )
+                 
+                }
+        }
+       
     }
 }
 
